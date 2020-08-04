fff3414aa4bc16d1887ad4f8be71403401b8a5eb
500inthegraveyard <hoostasr@gmail.com>
2020-08-04 17:59:33 -0500

    push with routes folder

---
+++ Develop/routes/workoutRoute.js
@@ -0,0 +1,11 @@
+const db = require("../models");
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });

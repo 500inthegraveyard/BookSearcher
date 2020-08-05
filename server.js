7ad3844a44cbe3d00f3dfffe066af8012d125455
500inthegraveyard <hoostasr@gmail.com>
2020-08-05 09:12:19 -0500

    with/workout for mongod db

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const { MongoClient } = require("mongodb");
 
 // Connection URI
 const uri =
-  "mongodb://localhost:27017";
+  "mongodb://localhost/workout:27017";
 
 // Create a new MongoClient
 const client = new MongoClient(uri);

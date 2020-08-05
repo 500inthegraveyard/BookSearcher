1d14e97cf52e9958bdd7113d9c2939e98e95fc52
500inthegraveyard <hoostasr@gmail.com>
2020-08-04 21:35:09 -0500

    completed models

--- models/workout.js
+++ models/workout.js
@@ -2,6 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
+    _id:Number,
+    day: Number,
+    
+    exercises:[{
+    type: String,
+    name: String,
+    duration: Number,
+    weight: Number,
+    reps: Number,
+    sets:Number}]
     
 })
 
